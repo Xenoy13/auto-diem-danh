dee1228e99cc8efbd2bbdfb9acf7a1f405e31fc7
Xenoy13 <hieucyberwork@gmail.com>
2025-10-26 07:48:28 +0700

    update bot

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from selenium import undetected_chromedriver as uc
+import undetected_chromedriver as uc
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
@@ -37,15 +37,14 @@ def send_telegram(message):
 # ==== HÀM ĐIỂM DANH ====
 def check_in():
     try:
-        chrome_options = Options()
-        chrome_options.add_argument("--headless")
-        chrome_options.add_argument("--no-sandbox")
-        chrome_options.add_argument("--disable-dev-shm-usage")
-        chrome_options.add_argument("--disable-gpu")
-        chrome_options.add_argument("--window-size=1920,1080")
-        chrome_options.add_argument("--remote-debugging-port=9222")  # fix crash Railway
-
-        driver = uc.Chrome(options=chrome_options)
+ chrome_options = uc.ChromeOptions()
+chrome_options.add_argument("--headless=new")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument("--disable-blink-features=AutomationControlled")
+chrome_options.add_argument("--window-size=1920,1080")
+driver = uc.Chrome(options=chrome_options)
         driver.get(URL)
 
         wait = WebDriverWait(driver, 15)
